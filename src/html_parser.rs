ec99ac68b940393ddaa7f9c690cb84f6067806b6
NamSoo Cho <joe@NamSooui-MacBook-Pro.local>
2017-04-02 13:32:32 +0900

    html parser on going

--- src/html_parser.rs
+++ src/html_parser.rs
@@ -7,5 +7,13 @@ enum ParserType { NONE = 0, LINK_URL = 0x01, FRAME_SRC = 0x02, BODY_TEXT = 0x04,
 
 pub struct HtmlParser {
     parser_type_: ParserType,
+    state_: State,
+    url_list: vec![String],
+    frame: vec![String],
+    plain_text: String,
+    is_body: bool,
+}
 
+impl HtmlParser {
+    
 }
