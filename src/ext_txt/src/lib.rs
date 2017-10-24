da2b369a64ca63df33438cf22e7a1c554c2e9808
NamsooCho <nscho66@gmail.com>
2017-10-24 17:15:56 +0900

    make skeleton

--- src/ext_txt/src/lib.rs
+++ src/ext_txt/src/lib.rs
@@ -8,5 +8,33 @@ use domain_dic::domainDic
 pub struct html_extractor
 {
     dic_ : domainDic,
+    startFlag_ : String,
+    endFlag_ : String,
+}
 
+impl html_extractor
+{
+    pub fn new () -> html_extractor
+    {
+        let mut h = html_extractor
+        {
+            dic_ : domainDic::new(),
+            startFlag_ : "",
+            endFlag_ : "",
+        }
+
+        h
+    }
+
+    fn getTags (&mut self, domain : String) -> Result<()>
+    {
+
+    }
+
+    pub fn extract (&mut self, org_txt : String, domain : String) -> String
+    {
+        let mut rlt_str : String = '';
+
+        getTags (domain);
+    }
 }
