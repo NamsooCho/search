5ed27a4c8d1dcf2f2cc89917068f8e1fc9335c42
NamSoo Cho <nscho66@gmail.com>
2017-10-24 20:37:09 +0900

    parsing

--- src/ext_txt/src/domain_dic.rs
+++ src/ext_txt/src/domain_dic.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-struct StartEndPair
+pub struct StartEndPair
 {
     start_flag_ : String,
     end_flag_ : String,
@@ -25,5 +25,25 @@ impl domainDic
     pub fn buildDic(&self) -> domainDic
     {
         
+    }
+
+    pub fn scanDic(&self, domain : String) -> StartEndPair
+    {
+        let search_rlt;
+        if self.cutter_.contains_key(&domain)
+        {
+            search_rlt = self.cutter_[&domain];
+        }
+        else 
+        {
+            search_rlt = StartEndPair {"", ""};
+        }
+
+        let pair = StartEndPair {
+            start_flag_ : search_rlt.start_flag_,
+            end_flag_ : search_rlt.end_flag_,
+        }
+
+        pair
     }
 }
--- src/ext_txt/src/lib.rs
+++ src/ext_txt/src/lib.rs
@@ -1,6 +1,10 @@
 // crate domain_dic
-mod domain_dic
-use domain_dic::domainDic
+
+use std::result::Result;
+use std::str;
+
+mod domain_dic;
+use domain_dic::domainDic;
 
 // build dictionary, search dictionary, extract html tag from html text string
 // get domain from caller
@@ -26,15 +30,49 @@ impl html_extractor
         h
     }
 
-    fn getTags (&mut self, domain : String) -> Result<()>
+    fn getFlags (&mut self, domain : String) -> Result<_,_>
     {
+        let pair = self.dic_.scanDic (domain);
+        self.startFlag_ = pair.startFlag_;
+        self.endFlag_ = pair.endFlag_;
+        let rlt;
+        if (self.startFlag_ == "" && self.endFlag_ == "")
+        {
+            rlt = Err();
+        }
+        else 
+        {
+            rlt = Ok();
+        }
 
+        rlt
     }
 
-    pub fn extract (&mut self, org_txt : String, domain : String) -> String
+    fn parse (&self, org_txt : &String) => String
+    {
+        let data = org_txt.as_bytes();
+        let data_str = str::from_utf8 (data).unwrap();
+        let mut b: usize = 0;
+        let e: usize = b + data.len();
+
+        let mut rlt = Vec::new();
+
+        if data_str.contains(&self.startFlag_);
+        {
+            
+        }
+    }
+
+    pub fn extract (&mut self, org_txt : &String, domain : String) -> String
     {
         let mut rlt_str : String = '';
 
-        getTags (domain);
+        match self.getFlags (domain)
+        {
+            Ok => {rlt_str = self.parse (org_txt);},
+            Err => {return ""},
+        };
+
+        rlt_str
     }
 }
