ab83622a9d833b6b00bfd52e6ea3509675574b33
NamSoo Cho <nscho66@gmail.com>
2017-10-30 20:47:31 +0900

    rewrite get_sock_addr

--- src/dns.rs
+++ src/dns.rs
@@ -17,37 +17,36 @@ impl Dns {
     pub fn get_sock_addr (&mut self, host: &String) -> Option<SocketAddrV4> {
         let sock_v4 : SocketAddrV4;
         let e : SocketAddrV4;
+
         match self.cache_.get (host) {
             Some(a) => {
                 e = a.clone();
-                sock_v4 = a;
+                sock_v4 = *a;
             },
             None => {
-                let addrs : Vec;
+                let addrs : Vec<SocketAddr>;
                 match net::lookup_host (&host) {
-                    Ok(a) => {
-                        addrs = a.filter (|s| s.is_ipv4()).collect(); 
-                    },
-                    Err(msg) => { addrs = vec![]; error! ("lookup host error. {}", msg);},
-                };
-
-                if addrs.len() == 0
-                {
-                    e = SocketAddrV4::new(Ipv4Addr::new(127,0,0,1), 80);
-                }
-                else 
-                {
-                    for sock_v4_6 in addrs.into_iter()
+                    Ok(a) => 
                     {
-                        match sock_v4_6 {
-                            SocketAddr::V4(x) => { e = x; break; },
-                            SocketAddr::V6(_) => { 
-                                e = SocketAddrV4::new(Ipv4Addr::new(127,0,0,1), 80);
-                                break;
-                            },
-                        };
-                    }
-                }
+                        addrs = a.filter (|s| s.is_ipv4()).collect();
+                        if addrs.len() != 0
+                        {
+                            match addrs[0] 
+                            {
+                                SocketAddr::V4(x) => { e = x;},
+                                SocketAddr::V6(_) => { 
+                                    e = SocketAddrV4::new(Ipv4Addr::new(127,0,0,1), 80);
+                                },
+                            };
+                        }
+                        else 
+                        {
+                            e = SocketAddrV4::new(Ipv4Addr::new(127,0,0,1), 80);
+                        }
+                    },
+                    Err(msg) => { e = SocketAddrV4::new(Ipv4Addr::new(127,0,0,1), 80); 
+                                error! ("lookup host error. {}", msg);},
+                };
                 sock_v4 = e.clone();
             },
         };
