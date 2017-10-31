9a6fe9b791095afe7e521b17510deb11dac6380e
NamsooCho <nscho66@gmail.com>
2017-10-31 09:18:14 +0900

    create variables and init on same line

--- src/dns.rs
+++ src/dns.rs
@@ -36,7 +36,8 @@ impl Dns {
                     };
                 }
             },
-        }
+        };
+        
         if sock_v4 == SocketAddrV4::new(Ipv4Addr::new(127,0,0,1), 80) {
             return None;
         }
