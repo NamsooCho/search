8048281f3d81427673d93be410e1a933ff3cceea
NamSoo Cho <nscho66@gmail.com>
2017-11-04 14:10:35 +0900

    hope this may be more advanced revision

--- src/dns.rs
+++ src/dns.rs
@@ -15,34 +15,30 @@ impl Dns {
     }
 
     pub fn get_sock_addr (&mut self, host: &String) -> Option<SocketAddrV4> {
-        let sock_v4 = match self.cache_.get(host) {
-            Some(addr) => Some(*addr),
+        let addr_rlt = match self.cache_.get(host) {
+            Some(addr) => {
+                return Some(*addr);
+            },
             None => {
-                let addrs : Vec<SocketAddr> = match net::lookup_host (&host) {
-                    Ok(a) => a.filter (|s| s.is_ipv4()).collect(),
-                    Err(msg) => { error! ("lookup host error. {}", msg); vec![] },
-                };
-
-                if addrs.len() == 0
-                {
-                    None
-                }
-                else 
-                {
-                    match addrs[0] 
-                    {
-                        SocketAddr::V4(x) => Some(x),
-                        SocketAddr::V6(_) => None,
-                    }
+                match net::lookup_host (&host) {
+                    Ok(a) => {
+                        let addrs: Vec<SocketAddr> = a.collect();
+                        let addr = match addrs[0] {
+                            SocketAddr::V4(x) => Some(x),
+                            SocketAddr::V6(_) => None,
+                        };
+                        addr
+                    },
+                    Err(msg) => {
+                        error! ("lookup host error. {}", msg);
+                        None
+                    },
                 }
             },
         };
-        
-        if sock_v4 != None
-        {
-            self.cache_.insert (host.clone(), sock_v4.clone().unwrap());
-        }
-        sock_v4
+
+        self.cache_.insert (host.clone(), addr_rlt.unwrap().clone());
+        addr_rlt
     }
 }
 
